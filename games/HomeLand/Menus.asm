488e5081319d28c34b147986c40e3edaa172482c
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2022-02-13 12:56:01 -0600

    Sorting of menus + translations

--- games/HomeLand/Menus.asm
+++ games/HomeLand/Menus.asm
@@ -13,9 +13,7 @@ Text($43B56130, "Team Up") ; fill 5
 
 //Item Menu
 Text($43B5620C, "I do not have any items") ; fill 5
-
 Text($43B56228, "Use") ; fill 5
-
 Text($43B56240, "Drop") ; fill 4
 Text($43B56248, "Give") ; fill 8
 
@@ -32,79 +30,62 @@ Text($43B564EC, "BGM Volume") ; fill 10
 Text($43B56500, " SE Volume") ; fill 6
 
 //Pause Menu
+Text($001160F8, "                    Saving") ; fill 1
+Text($00116180, "Adventure has been saved.") ; fill 3
 Text($00116C90, "Save") ; fill 5
-Text($00116C9C, "Break") ; fill 6
+Text($00116C9C, "Quit") ; fill 7
 Text($00116CD0, "Settings") ; fill 1
+Text($00116CE8, "Save adventure to memory card?") ; fill 2
+Text($00116D08, "Save adventure to memory card?") ; fill 2
+Text($00116D78, "Save and exit to the Main Menu?") ; fill 1
+Text($00116DFC, "Would you like to save and return home?") ; fill 4
 Text($00199E28, "History") ; fill 1
 Text($0019A198, "History") ; fill 1
 
-Text($00116DFC, "Would you like to save and return home?") ; fill 4
-Text($00116CE8, "Save adventure to memory card?") ; fill 2
-Text($00116D08, "Save adventure to memory card?") ; fill 2
-Text($00116180, "Adventure has been saved.") ; fill 3
-
-
-// Game Menus
-Text($41C05442, "Yes") ; fill 2
 Text($00199BE0, "Yes") ; fill 2
 Text($00199C70, "Yes") ; fill 2
+Text($41C05442, "Yes") ; fill 2
 
-Text($41C05447, "No") ; fill 5
-Text($00199D78, "No") ; fill 5
 Text($00199BE8, "No") ; fill 5
 Text($00199C5F, "No") ; fill 5
-Text($499D61F9, "No") ; fill 5
-Text($499D5516, "No") ; fill 5
-Text($499D5210, "No") ; fill 5
-Text($499D5137, "No") ; fill 5
-Text($499D5057, "No") ; fill 5
-Text($499D4F7B, "No") ; fill 5
-Text($499D4E5B, "No") ; fill 5
-Text($499D4A65, "No") ; fill 5
-Text($499D418F, "No") ; fill 5
-Text($499D40D4, "No") ; fill 5
-Text($499D3BB9, "No") ; fill 5
-Text($41BD646B, "No") ; fill 5
-Text($3EE6D7BF, "No") ; fill 5
-Text($3EA74D12, "No") ; fill 5
-Text($3EA73F77, "No") ; fill 5
-Text($3CF906CC, "No") ; fill 5
-Text($3CF8DD48, "No") ; fill 5
 Text($00199C60, "No") ; fill 5
+Text($00199D78, "No") ; fill 5
+Text($3CF8DD48, "No") ; fill 5
+Text($3CF906CC, "No") ; fill 5
+Text($3EA73F77, "No") ; fill 5
+Text($3EA74D12, "No") ; fill 5
+Text($3EE6D7BF, "No") ; fill 5
+Text($41BD646B, "No") ; fill 5
+Text($41C05447, "No") ; fill 5
+Text($499D3BB9, "No") ; fill 5
+Text($499D40D4, "No") ; fill 5
+Text($499D418F, "No") ; fill 5
+Text($499D4A65, "No") ; fill 5
+Text($499D4E5B, "No") ; fill 5
+Text($499D4F7B, "No") ; fill 5
+Text($499D5057, "No") ; fill 5
+Text($499D5137, "No") ; fill 5
+Text($499D5210, "No") ; fill 5
+Text($499D5516, "No") ; fill 5
+Text($499D61F9, "No") ; fill 5
 
 
+//Mascot
 Text($43ADDF6C, "Block") ; fill 7
-
 Text($43ADDF90, "Pinnock") ; fill 5
-
 Text($43ADDFD0, "Ganto") ; fill 3
-
 Text($43B242B4, RED) ; db "B Button", BLACK, ": Quit & Return" ; fill 11
-
 Text($43B3D704, "Exchange Item") ; fill 3
-
 Text($43B43370, "Call Somone") ; fill 4
-
 Text($43B434B8, "Guts") ; fill 4
-
 Text($43B43F18, "Loud voice reaches around the world") ; fill 1
-
 Text($43B44194, "Increase force") ; fill 2
-
 Text($43B4AC37, "Pow") ; fill 6
-
-TextShiftJIS($43B54790, "Joining") ; fill 2
-
 Text($43B55730, "    Sort Items") ; fill 2
-
+TextShiftJIS($43B54790, "Joining") ; fill 2
 TextShiftJIS($43B55B90, "Thinking") ; db $81, $63 ; fill 2
-
 TextShiftJIS($43B55BEC, "ForceUp+") ; db $25, $44 ; fill 2
-
 TextShiftJIS($43B55EF0, "ForceNormal") ; fill 2
-
-//Break Prompt
-Text($00116D78, "Save and exit to the Main Menu?") ; fill 1
 
 Text($43B56570, "[Block Mascot]\n")
              db " The Block is part of a tribe of toys who live in a town.\n"
@@ -119,7 +100,9 @@ Text($43B56A80, "[Ganto Mascot]\n")
              db " It has outstanding offensive & destructive power." ; fill 10
 
 Text($43B56D18, "There is no Mascot here yet.") ; fill 8
+Text($43B56D3C, "I'm already on an adventure") ; fill 1
 
+//Stats menu
 Text($43B58B90, "Clear Points") ; fill 4
 Text($43B58BA0, " points") ; fill 1
 Text($43B58BA8, "   Force:") ; fill 3
@@ -132,7 +115,6 @@ Text($43B58BEC, " Power:") ; fill 1
 Text($43B58BF4, "/ ") ; fill 2
 Text($43B58BF8, "Until Next Level:") ; fill 3
 Text($43B58C0C, "Current Completion:") ; fill 1
-
 Text($43B58C3C, " Level") ; fill 2
 Text($43B58C44, "     Exp:") ; fill 3
 
@@ -142,20 +124,15 @@ Text($57051B44, "Please name your character.") ; fill 1
 Text($57051B60, "You cannot use the same name which exists on this memory card.") ; fill 2
 Text($57051BA0, "Adventure will start with this name.") ; fill 4
 Text($57051BC8, "New adventure data has been stored on this memory card.") ; fill 7
-
 Text($57051D18, "%03d hours %02d mins") ; fill 4
-
 Text($57051F54, "Check memory card SlotA") ; fill 1
 Text($57051F6C, "Edit this memory card") ; fill 11
 Text($57051F8C, "Start without saving data") ; fill 23
 Text($57051FBC, "Is it OK to erase this data?") ; fill 8
 Text($57051FE0, "Are you sure about erasing this data?") ; fill 11
-
 Text($570520E8, "Is it OK to erase this data?") ; fill 8
 Text($5705210C, "Are you sure about erasing this data?") ; fill 11
-
 Text($57052168, "Failed to load this memory card") ; fill 9
-
 Text($570523A8, "  Play Time:") ; fill 8
 
 // Start Screen
@@ -168,7 +145,6 @@ Text($57052640, "Network") ; fill 9
 Text($57052650, "Network Setup") ; fill 3
 Text($57052660, "Connection Test") ; fill 1
 Text($57052670, "Credits") ; fill 13
-
 Text($57052684, "Go To The Network Settings.       \n")
 Text($570526A7, "Is This OK?") ; fill 14
 
@@ -178,24 +154,16 @@ Text($0011598C, "  OFF") ; db $81, $E8 ; fill 5
 Text($00115998, "It has been set") ; fill 5
 Text($001159AE, " Memorize") ; fill 5
 Text($001159BE, "  Reset") ; db $81, $E8 ; fill 7
-
 Text($00115B3C, "Please Wait") ; fill 13
-
 Text($00115CFC, "There is a problem with the memory card in SlotA.\n")
 Text($00115D2E, "It might get fixed by initializing it,     \n")
 Text($00115D5A, "but all the data will be lost.  \n")
 Text($00115D7B, "Do you want to initialize?") ; fill 3
-
 Text($00115DE8, "Initializing memory card. Is this OK?") ; fill 7
-
 Text($00115E6C, "It is now initialized.") ; fill 2
-
 Text($00115EC0, "It will not be able to save your progress in the game.") ; fill 6
-
 Text($00116084, "The memory card in Slot A is used to record the adventure.\n")
 Text($001160BF, "Please do not turn off the power or remove the card.") ; fill 5
 
-//Weird text box issue when saving
-Text($001160F8, "                    Saving") ; fill 1
-
+//Unsorted
 Text($3C08C6EA, "Insert") ; db RED ; fill 3 ; db " points ", BLACK, "and your mascot will\nfly away and become an assistant on your journey." ; fill 28
