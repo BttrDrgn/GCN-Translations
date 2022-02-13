2d3493c92a2d2383bc70e2d87c885945b55bb025
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2022-02-13 12:56:13 -0600

    More Bell dialogue

--- games/HomeLand/Story.asm
+++ games/HomeLand/Story.asm
@@ -255,6 +255,39 @@ Text($3C08C17F, "..........") ; db $FE, $14, "\n"
              db "Mr. " ; fill 1 ; db "........", $FE, $1E, "\n"
              db "Take care.", $FE, $32, $81, $A1 ; fill 1
 
-//Text($248799DB, "The people of this town often have issues.") ; fill 5
-//Text($24879A0E, "I hope you can resolve everyone's issue.") ; fill 13
-//Text($24879A47, "Big God will always be watching over") ; fill 2 ; " from the heavens." ; fill 13
+//Bell dialogue
+Text($3C08A752, "Hello ") ; fill 6
+Text($3C08A75E, "I'm  the one who will\nteach you the basics.") ; fill 9
+Text($3C08A792, "What do you need to know?") ; fill 2
+Text($3BFC3755, "What else do you want to know?") ; fill 4
+Text($3C08B16E, "What else do you want to know?") ; fill 4
+
+//-- Questions Box
+Text($3C08A7AE, "Where am I") ; fill 1
+Text($3C08A7B9, "Quester's Role") ; fill 4
+Text($3C08A7CC, "Various Quests") ; fill 2
+Text($3C08A7DD, "Objective") ; fill 5
+Text($3C08A7EC, "Go back home") ; fill 1
+Text($3C08A808, "Nothing") ; fill 3
+//--
+
+//-- Where am I
+Text($3C08A89A, "We call it ") ; db RED, "Mijikaku Park.", BLACK ; fill 19
+Text($3C08A8CB, "The park is connected to") ; fill 4
+Text($3C08A908, "the entire world.") ; fill 5
+//
+
+//-- Quester's Role
+Text($3C08A94C, "What is a Quester's role?\nIt is very simple.") ; db $81, $A5 ; fill 4
+Text($3C08A97E, "People all over the world have problems and you resolve them!") ; db $81, $A5; fill 2
+Text($3C08A9C3, "Listen carefully to people\nto solve their problems.") ; db $81, $A5 ; fill 24
+Text($3C08AA13, "The more problems you solve,\nthe more Big God will reward  you.") ; db $81, $A5 ; fill 4
+// 
+
+//First Quest dialogue
+//Angel
+Text($248799DB, "The people of this town often have issues.") ; fill 5
+Text($24879A0E, "I hope you can resolve everyone's issue.") ; fill 13
+Text($24879A47, "Big God will always be watching over") ; fill 2 ; db " from the heavens." ; fill 13
+
+//Kris
