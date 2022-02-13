a40b6f727010de7b08f5adda1b816d212c38c889
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2022-02-12 18:24:22 -0600

    More NPC names

--- games/HomeLand/NPC.asm
+++ games/HomeLand/NPC.asm
@@ -1,5 +1,16 @@
 Text($3C08A823, "Big God") ; fill 8
-Text($23E1CC00, "Tenshi") ; fill 1
-Text($3C08BE74, "Tenshi") ; fill 1
+Text($23E1CC00, "Angel") ; fill 1
+Text($3C08BE74, "Angel") ; fill 1
 Text($23E20CF8, "Mother") ; fill 5
 Text($3C08A74B, "Bell") ; fill 2
+Text($3C089E0B, "Hischin") ; fill 2
+Text($3C08B375, "Timote") ; fill 3
+Text($3C08B193, "Lyanse") ; fill 3
+Text($3C08A171, "Obada") ; fill 4
+
+Text($29446B66, "Patrol Angel") ; fill 3
+Text($26A748BB, "Patrol Angel") ; fill 3
+Text($272DF650, "Patrol Angel") ; fill 3
+Text($27C13C0F, "Patrol Angel") ; fill 3
+Text($28CCD0B0, "Patrol Angel") ; fill 3
+Text($3C08854C, "Patrol Angel") ; fill 3
