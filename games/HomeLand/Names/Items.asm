dc7767693dd9d7d76d6e75b4c9a6405c7c2378a6
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2022-02-15 12:50:19 -0600

    Item translations

---
+++ games/HomeLand/Names/Items.asm
@@ -0,0 +1,51 @@
+//
+Text($00199D88, "Money") ; fill 2
+Text($43ADA8E4, "Money") ; fill 2
+Text($43C8DDAC, "Money") ; fill 2
+Text($49B086F0, "Money") ; fill 2
+
+Text($43B57214, "%s : %s bol") ; fill 1
+Text($43CF1CCC, "%s : %s bol") ; fill 1
+//
+
+//
+Text($43ADA910, "Herbs") ; fill 4
+Text($43C8DDD8, "Herbs") ; fill 4
+Text($49B0871C, "Herbs") ; fill 4
+
+Text($43B3D72C, "Restores 70 Life") ; fill 8
+Text($43CDED14, "Restores 70 Life") ; fill 8
+//
+
+//
+Text($43ADA170, "Stick") ; fill 4
+Text($43C8D638, "Stick") ; fill 4
+Text($49B07F7C, "Stick") ; fill 4
+
+Text($43B3C85C, "Cheap and Simple Weapon") ; fill 5
+Text($43CDDE44, "Cheap and Simple Weapon") ; fill 5
+//
+
+Text($43ADA17C, "Oak Rod") ; fill 8
+Text($43C8D644, "Oak Rod") ; fill 8
+Text($49B07F88, "Oak Rod") ; fill 8
+
+Text($43ADA18C, "Bamboo Pole") ; fill 1
+Text($43C8D654, "Bamboo Pole") ; fill 1
+Text($49B07F98, "Bamboo Pole") ; fill 1
+
+Text($43ADA1B8, "Hand Fan") ; fill 1
+Text($43C8D680, "Hand Fan") ; fill 1
+Text($49B07FC4, "Hand Fan") ; fill 1
+
+Text($43ADA4C4, "Wood Shield") ; fill 1
+Text($43C8D98C, "Wood Shield") ; fill 1
+Text($49B082D0, "Wood Shield") ; fill 1
+
+Text($43ADA4D0, "Scrap Shield") ; fill 4
+Text($43C8D998, "Scrap Shield") ; fill 4
+Text($49B082DC, "Scrap Shield") ; fill 4
+
+Text($43ADA4E0, "Rock Shield") ; fill 1
+Text($43C8D9A8, "Rock Shield") ; fill 1
+Text($49B082EC, "Rock Shield") ; fill 1
