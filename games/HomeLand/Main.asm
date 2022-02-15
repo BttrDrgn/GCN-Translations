f987249cf8b66024c9cdc36e855959492d8cb860
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2022-02-15 12:44:57 -0600

    ShiftJIS without origin for continuation

--- games/HomeLand/Main.asm
+++ games/HomeLand/Main.asm
@@ -50,6 +50,26 @@ macro TextShiftJIS(OFFSET, TEXT) {
   dw {TEXT} // Shift-JIS Text To Print
 }
 
+macro TextShiftJISCont(TEXT) {
+  // Map Shift-JIS Words
+  map ' ',  $8140
+  map $2C,  $8143 // Comma ","
+  map '.',  $8144
+  map ':',  $8146
+  map '?',  $8148
+  map '!',  $8149
+  map '~',  $8160
+  //map '\s', $8166 // Single Quote "'"
+  //map '\d', $8168 // Double Quote '"'
+  map '+',  $817B
+  map '&',  $8195
+  map '0',  $824F, 10 // Map Numbers
+  map 'A',  $8260, 26 // Map English "Upper Case" Characters
+  map 'a',  $8281, 26 // Map English "Lower Case" Characters
+
+  dw {TEXT} // Shift-JIS Text To Print
+}
+
 //Region
 Text($3, "E")
 
