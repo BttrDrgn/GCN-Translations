4d884273cd1f447aaade82345627e33f1de1a899
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2022-02-15 14:45:58 -0600

    Renamed items based on Puutan translation

--- games/HomeLand/Names/Items.asm
+++ games/HomeLand/Names/Items.asm
@@ -1,3 +1,5 @@
+//There are multiple definitions per item because they are in alone.rel, client.rel, and server.rel, and maybe main.dol
+
 //
 Text($00199D88, "Money") ; fill 2
 Text($43ADA8E4, "Money") ; fill 2
@@ -18,34 +20,52 @@ Text($43CDED14, "Restores 70 Life") ; fill 8
 //
 
 //
-Text($43ADA170, "Stick") ; fill 4
-Text($43C8D638, "Stick") ; fill 4
-Text($49B07F7C, "Stick") ; fill 4
+Text($43ADA170, "Twig") ; fill 5
+Text($43C8D638, "Twig") ; fill 5
+Text($49B07F7C, "Twig") ; fill 5
 
 Text($43B3C85C, "Cheap and Simple Weapon") ; fill 5
 Text($43CDDE44, "Cheap and Simple Weapon") ; fill 5
 //
 
-Text($43ADA17C, "Oak Rod") ; fill 8
-Text($43C8D644, "Oak Rod") ; fill 8
-Text($49B07F88, "Oak Rod") ; fill 8
+//
+Text($43ADA17C, "Oak Stick") ; fill 10
+Text($43C8D644, "Oak Stick") ; fill 10
+Text($49B07F88, "Oak Stick") ; fill 10
+//
 
-Text($43ADA18C, "Bamboo Pole") ; fill 1
-Text($43C8D654, "Bamboo Pole") ; fill 1
-Text($49B07F98, "Bamboo Pole") ; fill 1
+//
+Text($43ADA18C, "Bamboo Rod") ; fill 2
+Text($43C8D654, "Bamboo Rod") ; fill 2
+Text($49B07F98, "Bamboo Rod") ; fill 2
+//
 
-Text($43ADA1B8, "Hand Fan") ; fill 1
-Text($43C8D680, "Hand Fan") ; fill 1
-Text($49B07FC4, "Hand Fan") ; fill 1
+//
+Text($43ADA1B8, "Fan") ; fill 6
+Text($43C8D680, "Fan") ; fill 6
+Text($49B07FC4, "Fan") ; fill 6
+//
 
+//Leather doesn't fit
 Text($43ADA4C4, "Wood Shield") ; fill 1
 Text($43C8D98C, "Wood Shield") ; fill 1
 Text($49B082D0, "Wood Shield") ; fill 1
+//
 
+//Scrap iron doesn't fit
 Text($43ADA4D0, "Scrap Shield") ; fill 4
 Text($43C8D998, "Scrap Shield") ; fill 4
 Text($49B082DC, "Scrap Shield") ; fill 4
+//
 
+//Stone doesn't fit
 Text($43ADA4E0, "Rock Shield") ; fill 1
 Text($43C8D9A8, "Rock Shield") ; fill 1
 Text($49B082EC, "Rock Shield") ; fill 1
+//
+
+//
+Text($43ADA4EC, "Steel Shield") ; fill 4
+Text($43C8D9B4, "Steel Shield") ; fill 4
+Text($49B082F8, "Steel Shield") ; fill 4
+//
