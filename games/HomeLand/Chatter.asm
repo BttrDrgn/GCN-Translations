e99e29ed8c93687984b2bc3b328efd2ee1dab3b2
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2022-02-15 12:49:54 -0600

    Battle text

--- games/HomeLand/Chatter.asm
+++ games/HomeLand/Chatter.asm
@@ -22,3 +22,18 @@ Text($35BB6061, "Hello, welcome to the inn.") ; Next() ; fill 17
 Text($373180E7, "Hello, welcome to the inn.") ; Next() ; fill 17
 Text($383FDA0D, "Hello, welcome to the inn.") ; Next() ; fill 17
 //End
+
+//God phone
+Text($41BD64EA, "    ") ; db RED, "[ Big God Phone ]        ", BLACK, NEWLINE
+Text($41C054C6, "    ") ; db RED, "[ Big God Phone ]        ", BLACK, NEWLINE
+
+//Battle messages
+TextShiftJIS($43B5439E, " Damage") ; fill 1
+TextShiftJIS($43CEF06E, " Damage") ; fill 1
+
+Text($43CF07DC, "%D") ; TextShiftJISCont(" BOL") ; fill 6
+Text($43B55AF8, "%D") ; TextShiftJISCont(" BOL") ; fill 6
+
+Text($43B55B08, "%D") ; TextShiftJISCont(" BOL : ") ; db $25, $44 ; TextShiftJISCont(" EXP") ; fill 2
+Text($43CF07EC, "%D") ; TextShiftJISCont(" BOL : ") ; db $25, $44 ; TextShiftJISCont(" EXP") ; fill 2
+//End Battle messages
