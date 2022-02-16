0989feaec811e6e658734259f4c9a4c5dfb5cae5
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2022-02-16 15:58:38 -0600

    Reorganized lupin

--- games/Lupin Disc 1/Banner.asm
+++ games/Lupin Disc 1/Banner.asm
@@ -1,6 +1,6 @@
 // Banner Image
 origin $31D3AE00
-insert "GFX/opening1.bnr" // Insert Banner Image File "opening.bnr" (6176 Bytes)
+insert "fs/opening1.bnr" // Insert Banner Image File "opening.bnr" (6176 Bytes)
 
 // Banner Text
 TextSave($31D3C620, "Lupin The Third - Disc 1") ; db 0,0,0,0,0,0,0,0 // Title Text (32 Bytes)
