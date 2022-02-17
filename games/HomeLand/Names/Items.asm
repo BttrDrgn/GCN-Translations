dba5c553d7387a7e9c1a8c28b201b914eee1cf21
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2022-02-16 20:57:34 -0600

    [HL] Added restorative item

--- games/HomeLand/Names/Items.asm
+++ games/HomeLand/Names/Items.asm
@@ -1,6 +1,6 @@
 //There are multiple definitions per item because they are in alone.rel, client.rel, and server.rel, and maybe main.dol
 
-//
+//Money
 Text($00199D88, "Money") ; fill 2
 Text($43ADA8E4, "Money") ; fill 2
 Text($43C8DDAC, "Money") ; fill 2
@@ -8,73 +8,65 @@ Text($49B086F0, "Money") ; fill 2
 
 Text($43B57214, "%s: %s boru") ; fill 1
 Text($43CF1CCC, "%s: %s boru") ; fill 1
-//
 
-//
-Text($43ADA910, "Herbs") ; fill 4
-Text($43C8DDD8, "Herbs") ; fill 4
-Text($49B0871C, "Herbs") ; fill 4
+//Herbs
+Text($43ADA910, "Herb") ; fill 5
+Text($43C8DDD8, "Herb") ; fill 5
+Text($49B0871C, "Herb") ; fill 5
 
 Text($43B3D72C, "Restores 70 Life") ; fill 8
 Text($43CDED14, "Restores 70 Life") ; fill 8
-//
 
-//
-
+//Antidote Herb
 Text($30B3DF4F, "Antidote Herb") ; fill 3
 Text($43ADAA14, "Antidote Herb") ; fill 3
 Text($43C8DEDC, "Antidote Herb") ; fill 3
 Text($49B08820, "Antidote Herb") ; fill 3
 
-//
+//Restorative
+Text($43ADA91C, "Restorative") ; fill 5
+Text($43C8DDE4, "Restorative") ; fill 5
+Text($49B08728, "Restorative") ; fill 5
 
-//
+//Twig
 Text($43ADA170, "Twig") ; fill 5
 Text($43C8D638, "Twig") ; fill 5
 Text($49B07F7C, "Twig") ; fill 5
 
 Text($43B3C85C, "Cheap and Simple Weapon") ; fill 5
 Text($43CDDE44, "Cheap and Simple Weapon") ; fill 5
-//
 
-//
+//Oak Stick
 Text($43ADA17C, "Oak Stick") ; fill 10
 Text($43C8D644, "Oak Stick") ; fill 10
 Text($49B07F88, "Oak Stick") ; fill 10
-//
 
-//
+//Bamboo Rod
 Text($43ADA18C, "Bamboo Rod") ; fill 2
 Text($43C8D654, "Bamboo Rod") ; fill 2
 Text($49B07F98, "Bamboo Rod") ; fill 2
-//
 
-//
+//Fan
 Text($43ADA1B8, "Fan") ; fill 6
 Text($43C8D680, "Fan") ; fill 6
 Text($49B07FC4, "Fan") ; fill 6
-//
 
 //Leather doesn't fit
 Text($43ADA4C4, "Wood Shield") ; fill 1
 Text($43C8D98C, "Wood Shield") ; fill 1
 Text($49B082D0, "Wood Shield") ; fill 1
-//
 
 //Scrap iron doesn't fit
 Text($43ADA4D0, "Scrap Shield") ; fill 4
 Text($43C8D998, "Scrap Shield") ; fill 4
 Text($49B082DC, "Scrap Shield") ; fill 4
-//
 
 //Stone doesn't fit
 Text($43ADA4E0, "Rock Shield") ; fill 1
 Text($43C8D9A8, "Rock Shield") ; fill 1
 Text($49B082EC, "Rock Shield") ; fill 1
-//
 
-//
+//Steel Shield
 Text($43ADA4EC, "Steel Shield") ; fill 4
 Text($43C8D9B4, "Steel Shield") ; fill 4
 Text($49B082F8, "Steel Shield") ; fill 4
-//
