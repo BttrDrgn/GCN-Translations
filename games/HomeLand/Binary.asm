133bbc10990196f0de48be029932e9d5fc7a4a12
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2022-02-19 20:39:09 -0600

    [HL] Added start.rel loader (non-working; decomp repo issue)

--- games/HomeLand/Binary.asm
+++ games/HomeLand/Binary.asm
@@ -1,1 +1,2 @@
-ReplaceAsset($0020300, "../../decomp/homeland/main.dol", 1558528)
+ReplaceAsset($00020300, "../../decomp/homeland/main.dol", 1558528)
+ReplaceAsset($5703D280, "../../decomp/homeland/start.rel", 109944)
