4d5ca9f7e0d6564ecff628dde9a9f9784ad46648
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2022-02-21 01:25:57 -0600

    [HL] Added alone and client rel from decomp

--- games/HomeLand/Binary.asm
+++ games/HomeLand/Binary.asm
@@ -1,2 +1,4 @@
 ReplaceAsset($00020300, "../../decomp/homeland/main.dol", 1558528)
+ReplaceAsset($4398D85C, "../../decomp/homeland/alone.rel", 2437760)
+ReplaceAsset($43BE0BCC, "../../decomp/homeland/client.rel", 1458051)
 ReplaceAsset($5703D280, "../../decomp/homeland/start.rel", 109948)
