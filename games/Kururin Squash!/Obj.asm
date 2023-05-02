85199ca8afe95ecac2911027d8e2a890d95d8eed
NewGBAXL <81663474+NewGBAXL@users.noreply.github.com>
2023-05-02 13:14:22 -0400

    [KS] Update Obj.asm
    Add in more files to patcher

--- games/Kururin Squash!/Obj.asm
+++ games/Kururin Squash!/Obj.asm
@@ -1,1 +1,8 @@
-ReplaceAsset($50431BA0, "fs/obj/2D/ano00.txg", 31168)
+ReplaceAsset($7CEC650, "fs/obj/2D/ano00.txg", 31168)
+ReplaceAsset($7D9A8B0, "fs/obj/2D/ch_c_resalt.txg", 1084960)
+ReplaceAsset($7EA36D0, "fs/obj/2D/ch_f_resalt.txg", 1084960)
+ReplaceAsset($863A870, "fs/obj/2D/option.txg", 931040)
+ReplaceAsset($8C3F430, "fs/obj/2D/resalt.txg", 1064448)
+ReplaceAsset($8DC9610, "fs/obj/2D/top.txg", 47712)
+ReplaceAsset($8E4E434, "fs/obj/2D/vs_menu.txg", 2146272)
+
